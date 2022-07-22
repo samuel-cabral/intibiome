46f92d429534876883c03eedfc5305ea2f18745e
Samuel Cabral <samuelcabral.mail@gmail.com>
2022-07-22 19:57:40 -0300

    Start Project

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/function-component-definition */
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return <h1>Hello World</h1>;
+};
+
+export default Home;
