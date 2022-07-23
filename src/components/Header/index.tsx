482ff18b82d2061da5d228b1ef1660867f01dee0
Samuel Cabral <samuelcabral.mail@gmail.com>
2022-07-23 15:59:44 -0300

    Add assets & component Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+export function Header() {
+  return (
+    <header>
+      <div>
+        <div>
+          <img src="/images/logos/logo.png" alt="intibiome" />
+        </div>
+      </div>
+    </header>
+  );
+}
