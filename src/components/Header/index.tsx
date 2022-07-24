57d6acd81b3619d0788c60b6171428330ee2cb05
Samuel Cabral <samuelcabral.mail@gmail.com>
2022-07-23 22:25:32 -0300

    Start component header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,28 @@
+import styles from './styles.module.scss';
+
 export function Header() {
   return (
-    <header>
-      <div>
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
         <div>
           <img src="/images/logos/logo.png" alt="intibiome" />
+          <div
+            style={{
+              display: 'flex',
+              alignContent: 'flex-end',
+              background: 'gray',
+            }}
+          >
+            <img src="/images/icons/search.svg" alt="Search Icon" />
+          </div>
         </div>
+
+        <nav>
+          <a>about us</a>
+          <a>our products</a>
+          <a>intimate health</a>
+          <a>contact us</a>
+        </nav>
       </div>
     </header>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,13 @@
 /* eslint-disable react/function-component-definition */
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { Header } from '../components/Header';
 
 const Home: NextPage = () => {
   return (
-    <>
-      <Head>
-        <title>intibiome</title>
-      </Head>
-      <h1>Hello World</h1>
-    </>
+    <Head>
+      <title>intibiome</title>
+    </Head>
   );
 };
 
